29ad257f1c4488a4d4a929d3619527e83f61bc69
AlaaAyesh <141708416+AlaaAyesh@users.noreply.github.com>
2025-05-01 23:27:30 +0300

    dml files

---
+++ examination_system/Insert Tables/dep_dml.sql
@@ -0,0 +1,17 @@
+---DML department
+
+use [db18494];
+go
+
+insert into dbo.department (name)
+values 
+    ('software dev'),
+    ('network admin'),
+    ('tech support'),
+    ('db admin'),
+    ('cybersecurity'),
+    ('devops'),
+    ('cloud compute'),
+    ('ui ux design'),
+    ('data analysis'),
+    ('it helpdesk');
