1118cd2222f8754f9cd3d9b779f244e4acec863f
Ahmed Taher <90255766+ahmedtaher45@users.noreply.github.com>
2025-05-02 18:47:51 +0300

    5-2-2025

--- examination_system/Stored procedures/Instructor make exam.sql
+++ examination_system/Stored procedures/Instructor make exam.sql
@@ -15,3 +15,45 @@ begin
 	inner join Exam E
 	on E.ID = SE.ExamID
 end
+go
+
+
+-- f
+create function GetExamQSForStd_FN(@StdID int)
+returns table
+as
+return
+(
+		select Q.Question
+		from QuestionPool Q inner join QuestionPick QP
+		on Q.ID = QP.QuestionID
+		inner join Exam E
+		on E.ID = QP.ExamID
+		inner join StudentExam SE
+		on E.ID = SE.ExamID
+		where SE.StdID = @StdID
+)
+
+go
+create or alter proc AllowStdToExam @StdID int
+as
+	begin
+			if exists(
+			select 1 
+			from Exam E
+			inner join StudentExam SE on E.ID = SE.ExamID
+			where SE.StdID = @StdID
+			  and E.[Date] = CAST(GETDATE() as date)
+			  and CAST(GETDATE() as Time(0)) between E.Start_Time and E.End_Time
+		)
+
+			begin
+				select * from GetExamQSForStd_FN(@StdID)
+			end
+		else
+			begin 
+				print 'Outside of exam time window.'
+			end
+	end
+
+AllowStdToExam 48
