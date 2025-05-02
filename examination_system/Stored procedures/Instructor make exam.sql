377275f715fba145534ff313e12377dd25c4e62b
Ahmed Taher <90255766+ahmedtaher45@users.noreply.github.com>
2025-05-03 02:11:25 +0300

    2:00

--- examination_system/Stored procedures/Instructor make exam.sql
+++ examination_system/Stored procedures/Instructor make exam.sql
@@ -6,26 +6,82 @@ WHERE type IN ('S', 'U', 'G') -- S = SQL user, U = Windows user, G = Windows gro
   AND name NOT IN ('dbo', 'guest', 'INFORMATION_SCHEMA', 'sys');
 go
 
+create function getCourseID(@CourseName nvarchar(30))
+returns int
+as
+	begin
+		declare @CourseID int
+		select @CourseID = ID
+		from Course
+		where Name = @CourseName;
 
-create proc sp_StudentExam @fname nvarchar(20),@lname nvarchar(20),@examID int,@date date,@start_time Time(0),@end_time Time(0)
+		return @CourseID;
+	end
+go
+
+create
+create proc sp_CreateExamAndType
+	@ExamType nvarchar(20),
+	@date date,
+	@start_time Time(0),
+	@end_time Time(0),
+	@TotalDegree int,
+	@CourseName nvarchar(30)
 as
 begin
-	select FName = @fname, LName = @lname, E.ID = @examID, E.[Date] = @date,E.Start_Time = @start_time ,E.End_Time = @end_time
-	from Person.Student S inner join StudentExam SE
-	on S.ID = SE.StdID
-	inner join Exam E
-	on E.ID = SE.ExamID
+	IF (USER!='db18494_Instructor' and 
+        BEGIN
+            RAISERROR('Access Denied! Only Instructors can make an Exam.', 16, 1);
+            RETURN;
+        END;
+
+	if(@end_time - @start_time > 2)
+		print 'The Exam duration must be two hours or less'
+	else if(@TotalDegree > 100)
+		begin
+			RAISERROR('Exam degree exceeds course limit!', 16, 1);
+			RETURN;
+		end
+	else if	(@CourseName not in (select [Name] from Course))
+		print 'Please select a valid Course Name'
+	else if(@ExamType not in ('Exam','Corrective'))
+		print 'The exam Type must be either Exam or Corrective'
+	else
+	begin
+		begin try
+			begin transaction
+			Insert into Exam
+			Values (Year(GETDATE()),@start_time ,@end_time,@date,@TotalDegree,
+						dbo.getCourseID(@CourseName))
+
+			declare @ExamID int = SCOPE_IDENTITY();
+			insert into ExamType values(@ExamID,@ExamType)
+			commit
+			print('Exam and ExamType inserted succeffully')
+		end try
+		begin catch
+			rollback
+			print 'An error occurred: '+ Error_MESSAGE();
+		end catch
+	end
 end
 go
 
+create 
 
 
--- f
+--from Person.Student S inner join StudentExam SE
+--	on S.ID = SE.StdID
+--	inner join Exam E
+--	on E.ID = SE.ExamID
+
+
+------------------------------------------------------------------------
 create function GetExamQSForStd_FN(@StdID int)
 returns table
 as
 return
-(
+( 
 		select Q.Question
 		from QuestionPool Q inner join QuestionPick QP
 		on Q.ID = QP.QuestionID
