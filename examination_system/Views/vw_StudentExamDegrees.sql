d993bdde69e0808316223c9e4c88e6191c7037b8
Ahmed Khaled <ahmed.khaled.sayed2000@gmail.com>
2025-05-02 16:13:22 +0300

    edite in student exam degree view

--- examination_system/Views/vw_StudentExamDegrees.sql
+++ examination_system/Views/vw_StudentExamDegrees.sql
@@ -1,10 +1,16 @@
-CREATE OR ALTER VIEW Person.vw_StudentExamDegrees
+CREATE OR ALTER VIEW Person.vw_StudentExamDegrees 
 AS
 SELECT 
-    FName + ' ' + LName AS [Student Full Name], 
-    StdExamDegree AS [Exam Degree]
-FROM Person.Student
-JOIN StudentExam
-ON StudentExam.StdID = Person.Student.ID;
+    S.FName + ' ' + S.LName AS [Student Full Name], 
+    SE.StdExamDegree AS [Exam Degree],
+    C.Name
+FROM Person.Student S
+JOIN StudentExam SE ON SE.StdID = S.ID
+JOIN StudentCourse SC ON SC.StudentID = S.ID 
+join Exam on Exam.CourseID = SC.CourseID
+JOIN Course C ON C.ID = SC.CourseID
+
 
 select * from Person.vw_StudentExamDegrees
+
+
