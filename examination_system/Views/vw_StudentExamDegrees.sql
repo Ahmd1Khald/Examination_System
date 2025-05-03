a1663472f9df1c37d10800d138b217beb14a9b76
Ahmed Khaled <ahmed.khaled.sayed2000@gmail.com>
2025-05-03 03:26:29 +0300

    edit in my view that show the degrees of student

--- examination_system/Views/vw_StudentExamDegrees.sql
+++ examination_system/Views/vw_StudentExamDegrees.sql
@@ -1,16 +1,12 @@
 CREATE OR ALTER VIEW Person.vw_StudentExamDegrees 
 AS
 SELECT 
-    S.FName + ' ' + S.LName AS [Student Full Name], 
-    SE.StdExamDegree AS [Exam Degree],
-    C.Name
+   S.FName + ' ' + S.LName AS [Student Full Name], 
+   SE.StdExamDegree AS [Exam Degree],
+   Course.Name
 FROM Person.Student S
 JOIN StudentExam SE ON SE.StdID = S.ID
-JOIN StudentCourse SC ON SC.StudentID = S.ID 
-JOIN Exam on Exam.CourseID = SC.CourseID
-JOIN Course C ON C.ID = SC.CourseID
+JOIN Course ON SE.CourseID = Course.ID
 
 
 select * from Person.vw_StudentExamDegrees
-
-
